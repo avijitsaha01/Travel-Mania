87953cb33fb841a96c825ab4f0b9c049c830d656
Avijit saha <avijitsahashovo2016@gmail.com>
2021-03-21 00:41:25 +0600

    update

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css'
+import gLogo from '../../assets/images/google.png'
 const Login = () => {
     return (
         <div className="container wrap">
@@ -10,19 +11,23 @@ const Login = () => {
                     <input type="password" placeholder="Password" />
                     <div className="option">
                         <div>
-                            <div class="form-check float-left">
-                                <input class="form-check-input" type="checkbox" value="" />
-                                <label class="form-check-label" for="defaultCheck1">
-                                     Remember Me
-                                </label>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
                             </div>
                         </div>
                         <div>
-                            <h5>Forgot Password</h5>
+                            <a href="/"><h5>Forgot Password</h5></a>
                         </div>
                     </div>
+                    <input className="loginBtn" type="submit" value="Log In"/>
+                    <span className="crtMsz">Don't have an account? <a href="/">Create an Account</a></span>
                 </form>
+                
+                
             </div>
+            <p className="or-style">Or</p>
+            <button className="googleBtn"><img src={gLogo} alt=""/>Continue with Google</button>
         </div>
     );
 };
