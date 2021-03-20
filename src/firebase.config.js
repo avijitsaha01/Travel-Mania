bcd6cd9961fd41f6d191b797915e5266122222a1
Avijit saha <avijitsahashovo2016@gmail.com>
2021-03-21 02:01:50 +0600

    update

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwuVd9oE4khuvMoiC8Qfv22LtaT5zyMfY",
+    authDomain: "react-auth-avijitsaha01.firebaseapp.com",
+    projectId: "react-auth-avijitsaha01",
+    storageBucket: "react-auth-avijitsaha01.appspot.com",
+    messagingSenderId: "363044891704",
+    appId: "1:363044891704:web:0a02fddc768fa8a8ba4acd"
+  };
+  export default firebaseConfig;
